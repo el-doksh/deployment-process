280c9f3bd6c8403e53d6d648fc5ebc6e96346ad8
Sherif Hesham <sherifeldoksh8@gmail.com>
2022-09-02 15:39:32 +0200

    update frontend and apis scripts

---
+++ udagram/udagram-api/bin/deploy.sh
@@ -0,0 +1,4 @@
+eb list 
+eb use udagram-api-dev
+eb deploy udagram-api-dev
+# eb setenv PORT=$PORT ENV=$ENV DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD DB_HOST=$DB_HOST DB_NAME=$DB_NAME DB_PORT=$DB_PORT
